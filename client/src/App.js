04df90ae24304621845511962e68452630a82d93
Steven Israel <malenchite@gmail.com>
2021-04-26 11:25:45 -0400

    Modified React socket to not use endpoint if ORIGIN is defined

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
-import socketIOClient from "socket.io-client";
+import io from "socket.io-client";
 import Chat from "./components/Chat";
 import "./App.css"
 
-const ENDPOINT = process.env.ORIGIN || "http://localhost:4001";
+const ENDPOINT = "http://localhost:4001";
 
 function App() {
   const [response, setResponse] = useState("");
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    const newSocket = socketIOClient(ENDPOINT);
+    const newSocket = process.env.ORIGIN ? io() : io(ENDPOINT);
     newSocket.on("clock", data => {
       setResponse(data);
     });
